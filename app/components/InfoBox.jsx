0d5eccad847ea2fc80c37fdf7cf89d215d15ba87
Vamsi Reddy <vamsi.reddy@daxko.com>
2026-05-11 18:04:17 +0530

    Toast msg to login on licking the add property

--- app/components/InfoBox.jsx
+++ app/components/InfoBox.jsx
@@ -1,13 +1,36 @@
-// import React from 'react'
-import Link from "next/link";
+"use client";
 
-const InfoBox = ({ title, description, link, buttonText, bgColor, buttonColor, hoverColor }) => {
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { toast } from "react-toastify";
+
+const InfoBox = ({
+  title,
+  description,
+  link,
+  buttonText,
+  bgColor,
+  buttonColor,
+  hoverColor,
+}) => {
+  const { data: session } = useSession();
+
+  const handleClick = (e) => {
+    if (buttonText === "Add Property" && !session) {
+      e.preventDefault();
+      toast.error("Login first to add property");
+    }
+  };
+
   return (
     <div className={`${bgColor} p-6 rounded-lg shadow-md`}>
       <h2 className="text-2xl font-bold">{title}</h2>
+
       <p className="mt-2 mb-4">{description}</p>
+
       <Link
         href={link}
+        onClick={handleClick}
         className={`inline-block ${buttonColor} text-white rounded-lg px-4 py-2 ${hoverColor}`}
       >
         {buttonText}
